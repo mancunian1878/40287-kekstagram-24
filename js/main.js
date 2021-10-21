4d747a9885718e879b05d9ea8e6f09667f017738
Murad <limatov.murad@yandex.ru>
2021-10-21 19:43:13 +0300

    задание 4.9 Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,20 +1,53 @@
-function getNumbers (from, to) {
-    for (let i = from; i <= to; i++) {
-     //result = (i=>0)  ? i : "число должно быть не меньше нуля" 
-        if (i >= 0) {
-            console.log(i);
-        } else {
-            console.log("число должно быть не меньше нуля");
-            break; 
-        }
-    };
-}
-getNumbers(0,200);
+const OBJESTS_TO_GEN = 25;
+const MESSAGE = [
+  'Всё отлично!', 'В целом всё неплохо. Но не всё',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра',
+  'В конце концов это просто непрофессионально.', 
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+const NAMES = ['Ирина', 'Ибрагим', 'Олег', 'Камилла'];
 
-function getMaxRangeComment (line, maxrange) {
-  if (line.length <= maxrange) {
-      return true;
-    }
-  return false;
+
+const createComment = (id) => ({
+  id,
+  avatar: `img/avatar-${Math.ceil(Math.random() * 6)}.svg`,
+  message: MESSAGE[_.random(0, MESSAGE.length - 1)],
+  name: NAMES[_.random(0, NAMES.length - 1)]
+})
+const createObject = (id) => {
+  const comments = []
+  const commentsCount = Math.ceil(Math.random() * 5)
+
+  for (let i = 1; i <= commentsCount; i++) {
+    comments.push(createComment(i))
+  }
+
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+
+  const result = Math.random() * (upper - lower + 1) + lower;
+  
+  return Math.floor(result);
 }
-getMaxRangeComment();
+
+
+  return {
+    id,
+    url: `photos/${id}.jpg`,
+    description: 'Мой первый опыт в фотографии',
+    likes: getRandomPositiveInteger(15, 200),
+    comments
+  }
+};
+
+
+
+const generateObjects = (count) => {
+  for (let i = 1; i <= count; i++) {
+    console.log(createObject(i))
+  }
+};
+
+generateObjects(OBJESTS_TO_GEN)
